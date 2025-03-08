439361180b51121dff9ac2181bf1fa117f11f2c3
MohammadMohsinKhan <mmohsink606@gmail.com>
2025-03-08 12:02:11 -0500

    Feat: Automating Unit tests

---
+++ src/frontend/jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
