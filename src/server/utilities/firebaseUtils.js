4be28b82b7fc737ba97aaf0e5228a1fabdcd13bd
Lucas Chen <lucas.chen2016@gmail.com>
2025-03-27 21:27:08 -0400

    fix: database

--- src/server/utilities/firebaseUtils.js
+++ src/server/utilities/firebaseUtils.js
@@ -3,6 +3,10 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import logger from "./loggerUtils.js";
+import { config } from "dotenv";
+
+// Load environment variables
+config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
